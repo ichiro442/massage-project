52a08705378bb8f80d6105f8b24701cedd60b304
ichiro422 <ichiro.and.juliet.and@gmail.com>
2021-03-11 07:23:14 +0900

    顧客ページcss完成

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -1,30 +1,40 @@
-
-
-
-
-<table>
-    <tr class="">
-        <th class="">顧客予約内容</th>
-    </tr>  
-    <tr>
-        <td>名前</td>
-        <td>{{ $customerDetail->name }}</td>
-    </tr>
-    <tr>
-        <td>電話番号</td>
-        <td>{{ $customerDetail->phoneNumber }}</td>
-    </tr>
-    <tr>
-        <td>メール</td>
-        <td>{{ $customerDetail->mail }}</td>
-    </tr>
-    <tr>
-        <td>予約日</td>
-        <td>{{ $customerDetail->calendar }}</td>
-    </tr>
-    <tr>
-        <td>予約時間帯</td>
-        <td>{{ $customerDetail->times }}</td>
-    </tr>
-</table>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
+    <title>顧客ページ</title>
+</head>
+<body>
+    <div class="customerContainer flex">
+        <div class="customerTitle flex">
+            <h2>顧客予約内容</h2>
+        </div>
+        <table class="customerList">
+            <tr>
+                <td>名前</td>
+                <td>{{ $customerDetail->name }}</td>
+            </tr>
+            <tr>
+                <td>電話番号</td>
+                <td>{{ $customerDetail->phoneNumber }}</td>
+            </tr>
+            <tr>
+                <td>メール</td>
+                <td>{{ $customerDetail->mail }}</td>
+            </tr>
+            <tr>
+                <td>予約日</td>
+                <td>{{ $customerDetail->calendar }}</td>
+            </tr>
+            <tr>
+                <td>予約時間帯</td>
+                <td>{{ $customerDetail->times }}</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
 
